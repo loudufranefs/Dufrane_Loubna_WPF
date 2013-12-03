6ddef8dba50997df32fab2647db424ed43ec320d
loudufraneFS <loudufrane@Lou.local>
2013-12-03 16:44:12 -0600

    slices of pie part 2 - calculation and result

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -22,17 +22,22 @@ console.log("Sparky is " + humanYears + " human years old which is " + dogYears 
 /*************SLICES OF PIE PART 1****************/
 
 //Number of slices per pizza
-var slices = 6;
+var slices = 8;
 //Number of people at the party
 var people =10;
 //Number of pizzas ordered
-var pizzas = 3;
+var pizzas = 4;
 
 //Calculated number of slices per person
 var slicesPerPerson = pizzas * slices / people;
 
-//print out result
+//print out result 
 console.log ("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
 
 
+/*************SLICES OF PIE PART 2****************/
+//calculate the number of slices per person and get the remainder.
+var leftOvers = pizzas * slices % people;
+
+console.log("Sparky got " + leftOvers + " slices of pizza.");
 

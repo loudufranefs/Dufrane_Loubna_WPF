6271e12ee41b2e197a52003ed4fcd4095377d48a
loudufraneFS <loudufrane@Lou.local>
2013-12-03 16:27:36 -0600

    Dog Years Calculation

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+/*
+Name: Loubna Dufrane
+Class: Web Programming Fundamentals
+Assignment: Expression Worksheet
+*/
+
+
+
+// DOG YEARS
+
+//set given for age in human years.
+var humanYears = 2;
+
+//calculate dog years based on the number given in humanYears variable.
+var dogYears = 7 * humanYears;
+
+//print out the result
+console.log("Sparky is " + humanYears + " human years old which is " + dogYears + " in dog years.");

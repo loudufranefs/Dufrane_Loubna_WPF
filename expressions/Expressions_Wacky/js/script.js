71d7bfd57bd49550ef11b0bf18e7c9b846d750a3
loudufraneFS <loudufrane@Lou.local>
2013-12-05 14:24:16 -0600

    Finished creating variables

--- expressions/Expressions_Wacky/js/script.js
+++ expressions/Expressions_Wacky/js/script.js
@@ -10,7 +10,13 @@ Description: Estimate how many Jelly Beans are in a Jar and win the contest.
 var jarHeight = prompt("Want to make a winning guess at a Jelly Bean in a Jar contest? \nWhat is the jar height in inches?");
 
 //get jar width
-var jarWidth = prompt ("What is the jar width in inches?");
+var jarWidth = prompt("What is the jar width in inches?");
 
 //get jellybean width
 var jellyBeanWidth = prompt("What is the length of the average jelly bean in inches.");
+
+//get jellybean width
+var jellyBeanWidth = prompt("What is the width of the average jelly bean in inches.");
+
+//get jellybean length
+var jellyBeanLength = prompt("What is the length of the average jelly bean in inches.");

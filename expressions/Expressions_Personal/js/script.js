661e1b070c745f65f3bdec18fb6c11c1664e82d2
loudufraneFS <loudufrane@Lou.local>
2013-12-05 11:29:29 -0600

    Changed calculation edited console log

--- expressions/Expressions_Personal/js/script.js
+++ expressions/Expressions_Personal/js/script.js
@@ -3,7 +3,7 @@ Name: Loubna Dufrane
 Class: Web Programming Fundamentals
 Assignment: Expressions - Balls of Yarn
 
-Description: Calculate how many balls of yarn to buy to complete an afghan based on size.
+Description: Calculate how many balls of yarn to complete an afghan based on desired width and length.
 */
 
 // yards per skein (ie. 140)
@@ -26,12 +26,12 @@ var length = prompt("Desired Length:");
 // calculate stiches per row
 var stitchesRow = stitchesInch * width;
 
-// calculate total stitches in inches
-var totalYarn = length * rowsInch * stitchesRow;
+// calculate total stitches in yards and convert inches to yards (1 yard = 36 inches)
+var totalYarn = (length * rowsInch * stitchesRow) / 36;
 
-// Convert inches to yards (1 yard = 36 inches) and calculate how many balls are needed.
-var skeinsNeeded = (totalYarn / 36) / skein;
+// Calculate how many balls are needed.
+var skeinsNeeded = totalYarn / skein;
 
 //print out Details
-console.log("To crochet an afghan that is " + width + " inches in width and " + length + " inches in length, you will need " + skeinsNeeded + " amount of balls of yarn." + "\n\nProject Details:\nOne Ball of Yarn is: " + skein + " yds\nWidth: " + width + " inches\nLength: " + length + " inches\nStiches Per Row: " + stitchesRow + "\nYarn needed: " + totalYarn + " inches\nBalls of Yarn: " + skeinsNeeded);
+console.log("To crochet an afghan that is " + width + " inches in width and " + length + " inches in length, you will need " + skeinsNeeded + " amount of balls of yarn that are " + skein + " yds each." + "\n\nProject Details:\nOne Ball of Yarn is: " + skein + " yds\nWidth: " + width + " inches\nLength: " + length + " inches\nStiches Per Row: " + stitchesRow + "\nYarn needed: " + totalYarn + " yds\nBalls of Yarn: " + skeinsNeeded);
 

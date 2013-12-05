32320b2233b9aa7099e4b16e4e76b8d442b20eb8
loudufraneFS <loudufrane@Lou.local>
2013-12-05 11:53:23 -0600

    declaring parenContainter variable

--- expressions/Expressions_Industry/js/script.js
+++ expressions/Expressions_Industry/js/script.js
@@ -5,3 +5,6 @@ Assignment: Expressions - Industry Specific
 
 Description: Simple Calculation converting Pixels to Percentages based on the CSS - box Model
 */
+
+//width of parent container in pixels
+var parentContainer = prompt("What is the width of the parent container in pixels?");

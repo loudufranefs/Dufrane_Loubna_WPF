53b1a3b5c672747310a590649995ff250804d4ee
loudufraneFS <loudufrane@Lou.local>
2013-12-05 12:41:35 -0600

    calculate container width

--- expressions/Expressions_Industry/js/script.js
+++ expressions/Expressions_Industry/js/script.js
@@ -3,7 +3,7 @@ Name: Loubna Dufrane
 Class: Web Programming Fundamentals
 Assignment: Expressions - Industry Specific
 
-Description: Simple Calculation converting Pixels to Percentages based on the CSS - box Model
+Description: Simple Calculation converting Pixels to Percentages based on the CSS box Model
 */
 
 // width of parent container in pixels
@@ -21,8 +21,10 @@ var margin = prompt("Margin in pixels:");
 // border in  pixels
 var border = prompt("Border in pixels:");
 
+// convert container width from pixels to percentages
+var containerWidth = 100 * (container / parentContainer);
 
 
 //print values in console for testing
-console.log("Parent Container: " + parentContainer + "px\nContainer: " + container + "px\nPadding: " + padding + "px\nMargin: " + margin + "px\nBorder: " + border + "px\nWidth outside Content: " + outsideContentWidth + "px");
+console.log("Parent Container: " + parentContainer + "px\nContainer: " + container + "px\nPadding: " + padding + "px\nMargin: " + margin + "px\nBorder: " + border + "px\nContainer Width: " + containerWidth + "%");
 

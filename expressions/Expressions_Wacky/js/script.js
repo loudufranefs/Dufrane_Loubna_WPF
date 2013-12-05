fb27477d82f1fa0f142559fa96661f172b3722e2
loudufraneFS <loudufrane@Lou.local>
2013-12-05 15:27:21 -0600

    made corrections to calculations and changed inches to cm for easier calculation

--- expressions/Expressions_Wacky/js/script.js
+++ expressions/Expressions_Wacky/js/script.js
@@ -7,16 +7,16 @@ Description: Estimate how many Jelly Beans are in a Jar and win the contest.
 */
 
 //get jar height
-var jarHeight = prompt("Want to make a winning guess at a Jelly Bean in a Jar contest? \nWhat is the jar height in inches?");
+var jarHeight = prompt("Want to make a winning guess at a Jelly Bean in a Jar contest? \nWhat is the jar height in cm?");
 
 //get jar width
-var jarWidth = prompt("What is the jar width in inches?");
+var jarWidth = prompt("What is the jar width in cm?");
 
 //get jellybean width
-var jellyBeanWidth = prompt("What is the length of the average jelly bean in inches.");
+var jellyBeanWidth = prompt("What is the width of the average jelly bean in cm.");
 
 //get jellybean length
-var jellyBeanLength = prompt("What is the length of the average jelly bean in inches.");
+var jellyBeanLength = prompt("What is the length of the average jelly bean in cm.");
 
 //add all my variables in an array.
 var widthHeight = [jarHeight, jarWidth, jellyBeanLength, jellyBeanWidth];
@@ -25,22 +25,22 @@ var widthHeight = [jarHeight, jarWidth, jellyBeanLength, jellyBeanWidth];
 var jarRaduis = widthHeight[1] / 2;
 console.log(jarRaduis);
 
-//calculate the volume of the jar in cubic inches -  πr2h
+//calculate the volume of the jar in cubic cm -  πr2h
 var jarVolume = 3.142 * (jarRaduis * jarRaduis) * widthHeight[0];
 
 //calculate the radius of a jellybean
 var jellyBeanRadius = widthHeight[3]/2;
 
-//calculate the size of an average jelly bean in cubic inches -  πr2h
+//calculate the size of an average jelly bean in cubic cm -  πr2h
 var jellyBeanSize = 3.142 * (jellyBeanRadius * jellyBeanRadius) * widthHeight[2];
 
 //calculate actual space the jellybeans are using in the jar. There is space in between solids in a jar, on average only taking up 70%.
 var spaceInJar = jarVolume * 0.7;
 
 //calculate how many jelly beans are in the jar.
-var totalJellyBeans = jellyBeanLength * spaceInJar;
+var totalJellyBeans = jellyBeanSize * spaceInJar;
 
 //print out result
-console.log("There are approximately " + totalJellyBeans + "amount of Jelly Beans inside this jar. This is an estimate only!");
+console.log("There are approximately " + totalJellyBeans + " Jelly Beans inside this jar. This is an estimate only! \n\n Details:\nJar Volume: " + jarVolume + " cubic cm \nAverage Jelly Bean Volume: " + jellyBeanSize + " cubic cm \nJelly Beans take up: " + spaceInJar + " cubic cm in the jar \n");
 
 

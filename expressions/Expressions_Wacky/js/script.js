ada0eaed00f801e8262970235b270208ed33540f
loudufraneFS <loudufrane@Lou.local>
2013-12-05 14:16:11 -0600

    Added 1st variable - jar height

--- expressions/Expressions_Wacky/js/script.js
+++ expressions/Expressions_Wacky/js/script.js
@@ -5,3 +5,6 @@ Assignment: Expressions - Wacky
 
 Description: Estimate how many Jelly Beans are in a Jar and win the contest.
 */
+
+//get the jar height
+var jarHeight = prompt("What is the jar height in inches");

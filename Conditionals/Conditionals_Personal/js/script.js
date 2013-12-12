fa4bfad55dbf7fc161508391954c4b005e991cff
loudufraneFS <loudufrane@Lou.local>
2013-12-12 17:18:51 -0600

    Validation for personal project

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -20,31 +20,51 @@ var food = prompt("How was your experience? \nRate the food from 1 to 10:"),
     service = prompt("Rate the service from 1 to 10"),
     price = prompt("What is the total bill? (without dollar sign)");
 
-//Find out if the food and the service was good
-if (food > 5 && service > 5) {
-    
-    // calculate how much the tip would be
-    var tip = Number(price) * 0.3;
-    //calculate total bill
-    var totalBill = Number(price) + tip;
-    
-    //print message that the food and the service were great
-    console.log("Thank you for lovely food and a wonderful service! \nI will leave a $" + tip + " tip, making my total bill $" + totalBill);
 
-} else if (food <= 5 && service <= 5) {
-    // if food and service are both bad
-    //print message that the food and the service were bad
-    console.log("I would like to speak to the manager!And I will not be leaving a tip! My total bill is $" + price);
-
-} else if (food <= 5 || service <= 5) {
-    //if food OR the service had a rating of equal to or under 5
-    
-    // calculate how much the tip would be
-    var tip = Number(price) * 0.15;
-    //calculate total bill
-    var totalBill = Number(price) + tip;
-    
-     //print message that the food and the service were ok
-    console.log("Thanks, it was ok, so I will leave a $" + tip + " tip, making my total bill $" + totalBill);
-
+//validation
+if (food === "") {
+    // checks if food is empty, if so prompts again
+    food = prompt("You forgot to rate the food:");
 }
+
+if (service === "") {
+    //checks if service is empty, if so prompt again
+    service = prompt("You forgot to rate the service:");
+}
+
+if (price === "") {
+    //checks if price is empty, if so prompt again
+    price = prompt("You forgot to add your total bill:");
+}
+
+
+ //if everything validates, continue
+
+    //Find out if the food and the service was good
+    if (food > 5 && service > 5) {
+        
+        // calculate how much the tip would be
+        var tip = Number(price) * 0.3;
+        //calculate total bill
+        var totalBill = Number(price) + tip;
+        
+        //print message that the food and the service were great
+        console.log("Thank you for lovely food and a wonderful service! \nI will leave a $" + tip + " tip, making my total bill $" + totalBill);
+    
+    } else if (food <= 5 && service <= 5) {
+        // if food and service are both bad
+        //print message that the food and the service were bad
+        console.log("I would like to speak to the manager!And I will not be leaving a tip! My total bill is $" + price);
+    
+    } else if (food <= 5 || service <= 5) {
+        //if food OR the service had a rating of equal to or under 5
+        
+        // calculate how much the tip would be
+        var tip = Number(price) * 0.15;
+        //calculate total bill
+        var totalBill = Number(price) + tip;
+        
+         //print message that the food and the service were ok
+        console.log("Thanks, it was ok, so I will leave a $" + tip + " tip, making my total bill $" + totalBill);
+    
+    }

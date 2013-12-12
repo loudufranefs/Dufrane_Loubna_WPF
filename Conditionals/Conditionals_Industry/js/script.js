41f4785fbaeff37f3c6e73b54d248cc06542efb3
loudufraneFS <loudufrane@Lou.local>
2013-12-12 15:22:03 -0600

    setting variables

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -9,3 +9,7 @@ Assignment: Conditionals - Industry Specific
 /*
 If a client wants to add WordPress as their content management to their website and they order within 5 days of their quote, then they get a 10% discount off their total order. Otherwise they only get a 5% discount if they order within 5 days of their quote, and no discount if their quote has expired.
 */
+
+//setting variables
+var wordPress = prompt("Would you like to Add WordPress to your website, type in Y for yes or N for No."),
+    daysLeft = prompt("How many days are left before your quote expires?");

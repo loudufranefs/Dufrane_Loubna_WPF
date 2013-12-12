a2c15c6ad3ede94afba1b2c6861ecd5ff4b4655d
loudufraneFS <loudufrane@Lou.local>
2013-12-12 15:26:14 -0600

    Check if client will order WordPress AND their quote is still valid

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -7,9 +7,16 @@ Assignment: Conditionals - Industry Specific
 */
 
 /*
-If a client wants to add WordPress as their content management to their website and they order within 5 days of their quote, then they get a 10% discount off their total order. Otherwise they only get a 5% discount if they order within 5 days of their quote, and no discount if their quote has expired.
+If a client wants to add WordPress as their content management to their website and they are ordering within 5 days of their quote, then they get a 10% discount off their total order. Otherwise they only get a 5% discount if they order within 5 days of their quote, and no discount if their quote has expired.
 */
 
 //setting variables
 var wordPress = prompt("Would you like to Add WordPress to your website, type in Y for yes or N for No."),
     daysLeft = prompt("How many days are left before your quote expires?");
+
+//Check if clients wants wordpress and quote is still valid
+if (wordPress === 'Y' && daysLeft <= 5) {
+    //print message with discount
+    console.log("Nice! You get a 10% Discount on your Total Order!");
+
+}

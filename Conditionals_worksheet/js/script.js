d6fbcbac724bf557fda78f55bf0c51775af360bf
loudufraneFS <loudufrane@Lou.local>
2013-12-12 14:11:40 -0600

    finalize temperature converter

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,20 +5,21 @@ Class: Web Programming Fundamentals
 Assignment: Conditionals Worksheet
 */
 
-/* Celsius to Fahrenheit converter */
+/* Group 1 - Celsius to Fahrenheit converter */
 
+//givens
 var degrees = prompt("Type in the temperature to convert (numbers only)"),
     unit = prompt("Type in F to convert to Fahrenheit or C to convert to Celsius"),
     convertC,
     convertF;
 
-//check if degree value is a number or not
+//check if degree value is a number
 if (degrees === NaN) {
-    //if the value for this variable is not a number, then give the user another chance to re-enter.
+    //if the value for is not a number prompt again
     degrees = prompt("Please enter a number for the degree you want to convert");
 }
     
-    
+//check which unit the user entered 
 if (unit === "C") {
     // if user enters "C" let's convert the number to celcius
     convertC = (degrees - 32) * 5 / 9;
@@ -32,10 +33,9 @@ if (unit === "C") {
         
     //print in console the converted value
     console.log("The temperature is " + convertF + " degrees Fahrenheit.");
-        
 
 } else {
     //If niether of those values were entered
-    unit = prompt("Please F for Fahrenheit or C for Celsius");
+    unit = prompt("Please type in F for Fahrenheit or C for Celsius in captial letters.");
 }
     

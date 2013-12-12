48338c3ad0098515498deeeda4a80b1a64aae820
loudufraneFS <loudufrane@Lou.local>
2013-12-12 13:06:06 -0600

    Celsius to Fahrenheit converter  givens

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,3 +4,8 @@ Date: Dec 12 2013
 Class: Web Programming Fundamentals
 Assignment: Conditionals Worksheet
 */
+
+/* Celsius to Fahrenheit converter */
+
+var degrees = prompt("Type in the degree to convert"),
+    unit = prompt("Type in F for Fahrenheit or C for Celsius");

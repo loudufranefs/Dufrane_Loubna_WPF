9b59716a7f1ea30a14329e5ee970be9c59ff673e
loudufraneFS <loudufrane@Lou.local>
2013-12-12 16:22:36 -0600

    Idea for Personal project - Personal Tip Calculator

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -3,6 +3,6 @@ Name: Loubna Dufrane
 Date: Dec 12 2013
 Class: Web Programming Fundamentals
 Assignment: Conditionals - Personal
-
 */
 
+/* Personal Tip calculator based on my personal standards */

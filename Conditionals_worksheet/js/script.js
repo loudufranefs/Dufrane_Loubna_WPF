f10c9fbcf711cc75462a40e9f8734de6dcfea65a
loudufraneFS <loudufrane@Lou.local>
2013-12-12 14:08:55 -0600

    changed calculation for fahrenheit - and changed conditional for number check

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,37 +7,35 @@ Assignment: Conditionals Worksheet
 
 /* Celsius to Fahrenheit converter */
 
-var degrees = prompt("Type in the degree to convert without unit."),
+var degrees = prompt("Type in the temperature to convert (numbers only)"),
     unit = prompt("Type in F to convert to Fahrenheit or C to convert to Celsius"),
     convertC,
     convertF;
 
 //check if degree value is a number or not
-if (degrees === isNaN) {
-    
+if (degrees === NaN) {
     //if the value for this variable is not a number, then give the user another chance to re-enter.
     degrees = prompt("Please enter a number for the degree you want to convert");
-    
-} else {
-    //the value for the variable degree is a number
+}
     
     
-    if (unit === "C") {
-        // if user enters "C" let's convert the number to celcius
-        convertC = (degrees - 32) * 5 / 9;
-        //print in console the converted value
-        console.log("The temperature is " + convertC + " degrees Fahrenheit.");
+if (unit === "C") {
+    // if user enters "C" let's convert the number to celcius
+    convertC = (degrees - 32) * 5 / 9;
+        
+    //print in console the converted value
+    console.log("The temperature is " + convertC + " degrees Celcius.");
     
-    } else if (unit === "F") {
-        // if user enters "F" let's convert the number to Fahrenheit
-        convertF = degrees * 5 / 9 + 32;
-        //print in console the converted value
-        console.log("The temperature is " + convertF + " degrees Celcius.");
+} else if (unit === "F") {
+    // if user enters "F" let's convert the number to Fahrenheit
+    convertF = degrees * 9 / 5 + 32;
+        
+    //print in console the converted value
+    console.log("The temperature is " + convertF + " degrees Fahrenheit.");
         
 
-    } else {
-        //If niether of those values were entered
-        unit = prompt("Please F for Fahrenheit or C for Celsius");
-    }
-    
+} else {
+    //If niether of those values were entered
+    unit = prompt("Please F for Fahrenheit or C for Celsius");
 }
+    

47e7cde7cbfc8a158f5702ceee732236bb97aadd
loudufraneFS <loudufrane@Lou.local>
2013-12-12 15:19:59 -0600

    Conditionals - industry specific story line

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -5,3 +5,7 @@ Class: Web Programming Fundamentals
 Assignment: Conditionals - Industry Specific
 
 */
+
+/*
+If a client wants to add WordPress as their content management to their website and they order within 5 days of their quote, then they get a 10% discount off their total order. Otherwise they only get a 5% discount if they order within 5 days of their quote, and no discount if their quote has expired.
+*/

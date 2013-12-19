02026788baf89c4c3333e95d7ac32bd41be1f29d
loudufraneFS <loudufrane@Lou.local>
2013-12-19 15:37:21 -0600

    Added idea for personal calculator

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -3,4 +3,7 @@ Name: Loubna Dufrane
 Date: Dec 13 2013
 Class: Web Programming Fundamentals
 Assignment: Functions Personal
+
+Description: Dog Food
+How many days would a bag of dog food last based on the size of the bag and the amount of food that is given to the dog each day. I have 2 dogs, and never remember how long a bag lasts for my dogs.
 */

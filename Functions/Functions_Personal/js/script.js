88b8a333b81005249cd88062c939045e11d7aa3d
loudufraneFS <loudufrane@Lou.local>
2013-12-19 15:41:22 -0600

    second variable

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -10,3 +10,6 @@ How many days would a bag of dog food last based on the size of the bag and the 
 
 // How much dog food does the bag hold
 var dogFood = prompt("How many pounds of dog food does the bag hold?");
+// how much food does the dog eat per day in cups
+var dailyIntake =  prompt("How much food does 1 dog eat per day? (in cups)");
+

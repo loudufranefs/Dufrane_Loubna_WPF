4f86f054184f9022bdebcc41d568b36cd7f60651
loudufraneFS <loudufrane@Lou.local>
2013-12-19 17:23:07 -0600

    product price

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -12,7 +12,9 @@ Description: Profit reach based on amount of sales or clients vs amount of hours
 var developmentHours = prompt("How many development hours went into this product?");
 var hourly = prompt("How much do you charge per hour in USD?");
 var sales = prompt("How many sales so far?");
+var productPrice = prompt("How much are you charging for the product?");
 
 //calculate how much the product costs in development hours
 var productCost = developmentHours * hourly;
+
 

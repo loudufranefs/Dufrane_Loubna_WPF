571b4abc07eb548ec1ddecda1b268602a260e3fe
loudufraneFS <loudufrane@Lou.local>
2013-12-19 17:16:53 -0600

    Idea for Industry

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -4,5 +4,7 @@ Date: Dec 19 2013
 Class: Web Programming Fundamentals
 Assignment: Functions Industry
 
+Description: Profit reach based on amount of sales or clients vs amount of hours it took to develop a product or render a service.
+
 */
 

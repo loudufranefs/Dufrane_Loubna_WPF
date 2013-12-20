1db76751a1382d359608bda7deec48c4b4d4465e
loudufraneFS <loudufrane@Lou.local>
2013-12-19 21:16:20 -0600

    Budget

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -7,3 +7,6 @@ Assignment: Functions Wacky
 Description: Find out how much to spend on christmas gifts based on budget.
 
 */
+
+//budget
+var budget = prompt("What is your Christmas budget this year?");

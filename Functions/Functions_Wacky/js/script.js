e1791931c15aad7c47c0d65500573e8c25b0238d
loudufraneFS <loudufrane@Lou.local>
2013-12-19 21:17:13 -0600

    adults

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -10,3 +10,5 @@ Description: Find out how much to spend on christmas gifts based on budget.
 
 //budget
 var budget = prompt("What is your Christmas budget this year?");
+//how many adults
+var adults = prompt("How many adults do you need to by a gift for?");

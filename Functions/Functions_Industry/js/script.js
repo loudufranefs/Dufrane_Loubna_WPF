902334f481bedf253002ae20e78bd5107c714278
loudufraneFS <loudufrane@Lou.local>
2013-12-19 18:01:35 -0600

    product costs are more than sales

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -15,22 +15,22 @@ var sales = prompt("How many sales so far?");
 var productPrice = prompt("How much are you charging for the product?");
 
 //function
-function profits(developmentHours, hourly, sales, productPrice){
-    
+function profits(developmentHours, hourly, sales, productPrice) {
+
     //calculate how much the product costs in development hours
     var productCost = developmentHours * hourly;
     //calculate sales in profit value
     var totalSales = sales * productPrice;
     // this will store the message for the return
-    var profitMessage ='';
-    
-    if (productCost < totalSales){ // if sales are more than product cost
+    var profitMessage = '';
+
+    if (productCost < totalSales) { // if sales are more than product cost
         //profit message if there is a profit
         profitMessage = "Awesome! You are in profit zone!!!";
-    } else if (productCost === totalSales){ // if sales and cost equal each other
+    } else if (productCost === totalSales) { // if sales and cost equal each other
         //profit message if sales and costs are equal
-        profitMessage"Looks like you broke even, but you are SO close...";
-    } else { // if there are no profits 
+        profitMessage = "Looks like you broke even, but you are SO close...";
+    } else if (productCost > totalSales) { // if there are no profits 
         //profit message if there are no profits
         profitMessage = "You haven't made a profit yet, keep trying!!";
     }
